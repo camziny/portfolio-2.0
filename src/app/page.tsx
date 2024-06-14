59d8d8433d965f2c8dceebcc380b20c710b24a72
camziny <cameronziny@gmail.com>
2024-06-14 13:56:57 -0400

    Add react three fiber
     - create 3d scene component
     - edit CamZiny header font and text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import ProjectCard from "./components/ProjectCard";
 import { ProjectsList } from "./data/projects";
 import CamZiny from "./components/CamZiny";
 import Contact from "./components/Contact";
+import ThreeDScene from "./components/ThreeDScene";
 
 export const metadata = {
   title: "Home - Cam Ziny",
@@ -10,8 +11,15 @@ export const metadata = {
 
 export default function Home() {
   return (
-    <div className="mt-32 container mx-auto px-4">
+    <div className="mt-32  sm:mt-0 container mx-auto px-4">
+      {/* <div className="py-10 sm:py-0 flex justify-center sm:justify-end">
+        <div className="absolute top-5 sm:relative sm:top-0">
+          <ThreeDScene />
+        </div>
+      </div> */}
+      {/* <div className="py-16 sm:py-0"> */}
       <CamZiny />
+      {/* </div> */}
       <section className="mt-12">
         <h2 className="text-center text-2xl font-bold text-gray-700 dark:text-white">
           Projects
